99cbb6ea54e1ded4fd19310bb7d49029d4a73c0d
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2025-10-10 10:36:28 +0100

    Initial commit

---
+++ src/main/java/org/neo4j/nodes/sdn/demo/NodesSdnDemoApplication.java
@@ -0,0 +1,13 @@
+package org.neo4j.nodes.sdn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodesSdnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodesSdnDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/neo4j/nodes/sdn/demo/NodesSdnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neo4j.nodes.sdn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodesSdnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
