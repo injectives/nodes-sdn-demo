462a82763ba0b9f652421df771cf35b0f5d3a110
Dmitriy Tverdiakov <dmitriy.tverdiakov@neo4j.com>
2025-10-15 10:16:12 +0100

    Enable all observability options

--- src/main/java/org/neo4j/nodes/sdn/demo/NodesSdnDemoApplication.java
+++ src/main/java/org/neo4j/nodes/sdn/demo/NodesSdnDemoApplication.java
@@ -1,7 +1,11 @@
 package org.neo4j.nodes.sdn.demo;
 
+import io.micrometer.observation.ObservationRegistry;
+import org.neo4j.driver.observation.micrometer.MicrometerObservationProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.neo4j.autoconfigure.ConfigBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NodesSdnDemoApplication {
@@ -10,4 +14,18 @@ public class NodesSdnDemoApplication {
         SpringApplication.run(NodesSdnDemoApplication.class, args);
     }
 
+    @Bean
+    ConfigBuilderCustomizer observationCustomizer(ObservationRegistry observationRegistry) {
+        return configBuilder -> {
+            var observationProvider = MicrometerObservationProvider.builder(observationRegistry)
+                    .alwaysIncludeQuery(true)
+                    .includeQueryParameters(true)
+                    .includeUrlScheme(true)
+                    .includeUrlTemplate(true)
+                    .requestHeaderPredicate(name -> true)
+                    .responseHeaderPredicate(name -> true)
+                    .build();
+            configBuilder.withObservationProvider(observationProvider);
+        };
+    }
 }
